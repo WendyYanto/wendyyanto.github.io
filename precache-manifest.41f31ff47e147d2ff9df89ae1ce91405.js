c219209736c05c17c1cdc3a189857a427b878f89
wendyyanto <yanto.wendy98@gmail.com>
2020-07-20 22:44:13 +0700

    deploy

---
+++ precache-manifest.41f31ff47e147d2ff9df89ae1ce91405.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378a0b553b203c00095a247d4573c1e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b226cb3bd364863b9e58",
+    "url": "/static/css/about-me.2b03dadf.chunk.css"
+  },
+  {
+    "revision": "902a530dd314cf8bcac8",
+    "url": "/static/css/button.adc145e5.chunk.css"
+  },
+  {
+    "revision": "403ec48691c4f3c39ced",
+    "url": "/static/css/cross.6588f0cc.chunk.css"
+  },
+  {
+    "revision": "e45996ef07c9f835ca4f",
+    "url": "/static/css/education-item.70d8918d.chunk.css"
+  },
+  {
+    "revision": "a553271d744365ce9d0b",
+    "url": "/static/css/education.ac3b60e5.chunk.css"
+  },
+  {
+    "revision": "bedc4166950f8e4c9d44",
+    "url": "/static/css/footer.2697051f.chunk.css"
+  },
+  {
+    "revision": "b5ee7a0794eaa40fe08b",
+    "url": "/static/css/hamburger.dcd4cdc0.chunk.css"
+  },
+  {
+    "revision": "d50d671b812a5d64a621",
+    "url": "/static/css/header.e2c2df4b.chunk.css"
+  },
+  {
+    "revision": "f1d745f226acafbeed07",
+    "url": "/static/css/logo.31a28c21.chunk.css"
+  },
+  {
+    "revision": "5d429d8b5363450a1ac4",
+    "url": "/static/css/main.a6ba8f92.chunk.css"
+  },
+  {
+    "revision": "6a5a33066cbd5ef477f4",
+    "url": "/static/css/navbar-item.1a95cce4.chunk.css"
+  },
+  {
+    "revision": "61c58ef3dd839d889444",
+    "url": "/static/css/navbar.14221797.chunk.css"
+  },
+  {
+    "revision": "fe204371b484891efbf8",
+    "url": "/static/css/porfolio.5e02bac0.chunk.css"
+  },
+  {
+    "revision": "f8aea45abf8b0753230b",
+    "url": "/static/css/portfolio-item.e763e921.chunk.css"
+  },
+  {
+    "revision": "879a3c7568d09235db6b",
+    "url": "/static/css/skill.9ac9c5fa.chunk.css"
+  },
+  {
+    "revision": "8267e026c15aab2af364",
+    "url": "/static/js/0.ab339f05.chunk.js"
+  },
+  {
+    "revision": "63ab52b265d8037b7a859e329f6fecca",
+    "url": "/static/js/0.ab339f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d01eba9fd839f3ba2e",
+    "url": "/static/js/17.cd43401a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/17.cd43401a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b226cb3bd364863b9e58",
+    "url": "/static/js/about-me.fc7bc994.chunk.js"
+  },
+  {
+    "revision": "902a530dd314cf8bcac8",
+    "url": "/static/js/button.9b25b9ec.chunk.js"
+  },
+  {
+    "revision": "403ec48691c4f3c39ced",
+    "url": "/static/js/cross.e8ea71b3.chunk.js"
+  },
+  {
+    "revision": "e45996ef07c9f835ca4f",
+    "url": "/static/js/education-item.1d38f38c.chunk.js"
+  },
+  {
+    "revision": "a553271d744365ce9d0b",
+    "url": "/static/js/education.b7e6e74f.chunk.js"
+  },
+  {
+    "revision": "bedc4166950f8e4c9d44",
+    "url": "/static/js/footer.03b16972.chunk.js"
+  },
+  {
+    "revision": "b5ee7a0794eaa40fe08b",
+    "url": "/static/js/hamburger.e2ebf455.chunk.js"
+  },
+  {
+    "revision": "d50d671b812a5d64a621",
+    "url": "/static/js/header.ad2e75e8.chunk.js"
+  },
+  {
+    "revision": "f1d745f226acafbeed07",
+    "url": "/static/js/logo.4b2b8c7c.chunk.js"
+  },
+  {
+    "revision": "5d429d8b5363450a1ac4",
+    "url": "/static/js/main.fd6a7953.chunk.js"
+  },
+  {
+    "revision": "6a5a33066cbd5ef477f4",
+    "url": "/static/js/navbar-item.31ee370e.chunk.js"
+  },
+  {
+    "revision": "61c58ef3dd839d889444",
+    "url": "/static/js/navbar.c0461511.chunk.js"
+  },
+  {
+    "revision": "fe204371b484891efbf8",
+    "url": "/static/js/porfolio.274511b5.chunk.js"
+  },
+  {
+    "revision": "f8aea45abf8b0753230b",
+    "url": "/static/js/portfolio-item.64887fbb.chunk.js"
+  },
+  {
+    "revision": "158788a0e815bf9c9f56",
+    "url": "/static/js/runtime-main.946e2228.js"
+  },
+  {
+    "revision": "879a3c7568d09235db6b",
+    "url": "/static/js/skill.c769a26a.chunk.js"
+  },
+  {
+    "revision": "33c301ed6ad4bbca34a305748051186b",
+    "url": "/static/media/bg-header.33c301ed.png"
+  },
+  {
+    "revision": "0bf2449c7091db287fcab39c39a99952",
+    "url": "/static/media/bg-path.0bf2449c.png"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/static/media/poppins.ba95810b.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-personal-website/precache-manifest.faf84876ce7e3e976beef369aedcebea.js"
+  "/precache-manifest.41f31ff47e147d2ff9df89ae1ce91405.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
